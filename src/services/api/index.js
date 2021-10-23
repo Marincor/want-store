b980c470fa3cab573613d70c18f08f02ee4d346b
Gabriel Claudino Marinho <gabriel.cmunivos@protonmail.com>
2021-10-23 15:22:00 -0300

    feat: 🎸 api integration sucess

---
+++ src/services/api/index.js
@@ -0,0 +1,16 @@
+const url =
+"https://magic-aliexpress1.p.rapidapi.com/api/bestSales/products?page=1&priceMax=20&priceMin=5&sort=EVALUATE_RATE_ASC&searchName=shoes";
+const key = process.env.NEXT_PUBLIC_PUBLISHABLE_KEY;
+
+export default async function getItems() {
+
+  
+  return await fetch(url, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "magic-aliexpress1.p.rapidapi.com",
+		"x-rapidapi-key": key
+	}
+}).then(res => res.json())
+
+}
