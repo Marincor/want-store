14987915bc86e42491347027fd9c3690f7ceee5d
Gabriel Claudino Marinho <gabriel.cmunivos@protonmail.com>
2021-10-26 14:02:34 -0300

    refactor: 💡 change states to actions of redux

---
+++ src/components/actions/dataItem/currentItem/index.js
@@ -0,0 +1,41 @@
+import getItemByID from "../../../../services/api/itemById";
+import { store } from "../../../../store";
+
+export default async function getCurrentItems(id) {
+
+    let finish = false;
+
+  try {
+
+    // set loadingAnimation //
+    store.dispatch({
+      type: "lOADING_STATE",
+      payload: true,
+    });
+
+    // API request and set items//
+    await getItemByID(id).then((data) =>{
+      store.dispatch({
+        type: "CURRENT_ITEM",
+        payload: data,
+      });
+
+      finish = data
+    }
+    );
+  } catch (error) {
+    console.log(error);
+  } finally {
+
+    // turn off animation loading //
+
+    if (finish.app_sale_price) {
+
+
+        store.dispatch({
+          type: "lOADING_STATE",
+          payload: false,
+        });
+    }
+  }
+}
---
+++ src/store/currentItem/index.js
@@ -0,0 +1,20 @@
+
+
+const initialState = {
+
+    item: null
+}
+
+
+
+export default function currentItemReducer(state = initialState, action) {
+
+    switch(action.type) {
+
+        case "CURRENT_ITEM": return action.payload;
+        break;
+        default: return state;
+        break;
+    }
+
+}
